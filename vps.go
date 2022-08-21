2efe5b301aa03f72182b1557c3672b639b7482a7
Josh Bayfield <61628835+jbayfield@users.noreply.github.com>
2022-08-21 15:47:03 +0100

    Power management functionality

--- vps.go
+++ vps.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 // GetVPSMap - Returns map of VPS on account
@@ -61,4 +62,52 @@ func (c *Client) DestroyVPS(identifier string, authToken *string) error {
 	}
 
 	return nil
+}
+
+// RebootVPS - Reboots specific VPS
+func (c *Client) RebootVPS(identifier string, authToken *string) error {
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/vps/servers/%s/reboot", c.HostURL, identifier), nil)
+	if err != nil {
+		return err
+	}
+
+	_, err = c.doRequest(req, authToken)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// powerMgmt - Common power management endpoint
+func powerMgmt(identifier string, action string, c *Client) error {
+	bodyReader := strings.NewReader(fmt.Sprintf("{ \"power\": \"%s\" }", action))
+
+	req, err := http.NewRequest("PUT", fmt.Sprintf("%s/vps/servers/%s/power", c.HostURL, identifier), bodyReader)
+	req.Header.Add("Content-Type", "application/json")
+	if err != nil {
+		return err
+	}
+
+	_, err = c.doRequest(req, &c.Token)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// PowerOnVPS - Power on VPS
+func (c *Client) PowerOnVPS(identifier string, authToken *string) error {
+	return powerMgmt(identifier, "power-on", c)
+}
+
+// PowerOffVPS - Power off VPS
+func (c *Client) PowerOffVPS(identifier string, authToken *string) error {
+	return powerMgmt(identifier, "power-off", c)
+}
+
+// ShutdownVPS - Power on VPS
+func (c *Client) ShutdownVPS(identifier string, authToken *string) error {
+	return powerMgmt(identifier, "shutdown", c)
 }
