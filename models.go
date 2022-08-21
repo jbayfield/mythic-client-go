87f9bcebdd0922623cc6330ba1e46bc97c1bc61b
Josh Bayfield <61628835+jbayfield@users.noreply.github.com>
2022-08-21 14:02:49 +0100

    Implement GetVPS

--- models.go
+++ models.go
@@ -2,8 +2,55 @@ package mythic
 
 // VPS -
 type VPS struct {
-	Name        string             `json:"name"`
-	Identifier  string             `json:"identifier"`
-	Product     string             `json:"product"`
-	Dormant     bool               `json:"dormant"`
+	// Basic properties - always present
+	Name       string `json:"name"`
+	Identifier string `json:"identifier"`
+	Product    string `json:"product"`
+	Dormant    bool   `json:"dormant"`
+
+	// Detailed properties
+	Status     string `json:"status,omitempty"`
+	HostServer string `json:"host_server,omitempty"`
+
+	Zone struct {
+		Code string `json:"code"`
+		Name string `json:"name"`
+	} `json:"zone,omitempty"`
+
+	CPUMode    string   `json:"cpu_mode,omitempty"`
+	NetDevice  string   `json:"net_device,omitempty"`
+	DiskBus    string   `json:"disk_bus,omitempty"`
+	Price      float32  `json:"price,omitempty"`
+	ISOImage   string   `json:"iso_image,omitempty"`
+	BootDevice string   `json:"boot_device,omitempty"`
+	IPv4       []string `json:"ipv4,omitempty"`
+	IPv6       []string `json:"ipv6,omitempty"`
+
+	Specs struct {
+		DiskType string `json:"disk_type"`
+		DiskSize int    `json:"disk_size"`
+		Cores    int    `json:"cores"`
+		RAM      int    `json:"ram"`
+	} `json:"specs,omitempty"`
+
+	MACs []string `json:"macs,omitempty"`
+
+	AdminConsole struct {
+		Username string `json:"username"`
+		Hostname string `json:"hostname"`
+	} `json:"admin_console,omitempty"`
+
+	SSHProxy struct {
+		Hostname string `json:"hostname"`
+		Port     int    `json:"port"`
+	} `json:"ssh_proxy,omitempty"`
+
+	VNC struct {
+		Mode     string `json:"mode"`
+		Password string `json:"password"`
+		IPv4     string `json:"ipv4"`
+		IPv6     string `json:"ipv6"`
+		Port     int    `json:"port"`
+		Display  int    `json:"display"`
+	} `json:"vnc,omitempty"`
 }
--- vps.go
+++ vps.go
@@ -27,3 +27,23 @@ func (c *Client) GetVPSMap(authToken *string) (map[string]VPS, error) {
 	return vpslist, nil
 }
 
+// GetVPS - Returns specific VPS
+func (c *Client) GetVPS(identifier string, authToken *string) (*VPS, error) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/vps/servers/%s", c.HostURL, identifier), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := c.doRequest(req, authToken)
+	if err != nil {
+		return nil, err
+	}
+
+	vps := VPS{}
+	err = json.Unmarshal(body, &vps)
+	if err != nil {
+		return nil, err
+	}
+
+	return &vps, nil
+}
